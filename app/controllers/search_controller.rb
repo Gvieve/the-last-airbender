24e47da0b7b617bcb203da7287493ca0d526b1fe
Gvieve <gnuebel13@gmail.com>
2021-04-05 11:02:24 -0600

    Add member info feature tests and to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,10 @@ class SearchController < ApplicationController
     conn = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters")
     response = conn.get("?perPage=100&page=1&affiliation=#{nation}")
     json = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+
     @total = json.count
     @characters = json[0..24].map do |data|
-      # require "pry"; binding.pry
+      Character.new(data)
     end
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -17,31 +17,53 @@ describe "As a user, when I visit '/search' page after selecting a search option
 
   describe "for each member's info" do
     before :each do
+      nation = 'fire+nation'
       conn = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters")
-      response = conn.get("?perPage=100&page=1&affiliation=#{nation}")
+      response = conn.get("?perPage=3&page=1&affiliation=#{nation}")
       json = JSON.parse(response.body, symbolize_names: true)
-      @members = json[0..24]
+      @members = json.map {|data| Character.new(data)}
+      @member1 = @members.first
+      @member2 = @members.second
+      @member3 = @members.last
     end
     it "I see their name and photo (if available)" do
-      @member
       visit search_path({nation: 'fire_nation'})
 
       within '.members-info' do
-        within ''
-        expect(page.all('li', count: 25))
+        within "#member-#{@member1.id}" do
+          expect(page).to have_content("Name: #{@member1.name}")
+        end
       end
     end
 
-    xit "I see a list of allies or 'None'" do
+    it "I see a list of allies or 'None'" do
+      visit search_path({nation: 'fire_nation'})
 
+      within '.members-info' do
+        within "#member-#{@member1.id}" do
+          expect(page).to have_content("Allies: #{@member1.allies}")
+        end
+      end
     end
 
-    xit "I see a list of enemies or 'None'" do
+    it "I see a list of enemies or 'None'" do
+      visit search_path({nation: 'fire_nation'})
 
+      within '.members-info' do
+        within "#member-#{@member1.id}" do
+          expect(page).to have_content("Enemies: #{@member1.enemies}")
+        end
+      end
     end
 
-    xit "Any affiliations that the member has" do
+    it "Any affiliations that the member has" do
+      visit search_path({nation: 'fire_nation'})
 
+      within '.members-info' do
+        within "#member-#{@member1.id}" do
+          expect(page).to have_content("Affiliation: #{@member1.affiliation}")
+        end
+      end
     end
   end
 end
